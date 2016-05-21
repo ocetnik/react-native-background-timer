db53d0942c8617eed7e195e6f3c598e45831100a
Doddy Sebastianus <doddy.sebastianus@expecc.com>
2016-05-21 20:00:32 +0700

    avoid null pointer when stop is called before start

--- android/src/main/java/com/ocetnik/timer/BackgroundTimerModule.java
+++ android/src/main/java/com/ocetnik/timer/BackgroundTimerModule.java
@@ -11,21 +11,21 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 import java.lang.Runnable;
 
 public class BackgroundTimerModule extends ReactContextBaseJavaModule {
-    
+
     private Handler handler;
     private ReactContext reactContext;
     private Runnable runnable;
-    
+
     public BackgroundTimerModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
     }
-    
+
     @Override
     public String getName() {
         return "BackgroundTimer";
     }
-    
+
     @ReactMethod
     public void start(final int delay) {
         handler = new Handler();
@@ -36,15 +36,16 @@ public class BackgroundTimerModule extends ReactContextBaseJavaModule {
                 handler.postDelayed(runnable, delay);
             }
         };
-        
+
         handler.post(runnable);
     }
-    
+
     @ReactMethod
     public void stop() {
-        handler.removeCallbacks(runnable);
+        // avoid null pointer exceptio when stop is called without start
+        if (handler != null) handler.removeCallbacks(runnable);
     }
-    
+
     private void sendEvent(ReactContext reactContext, String eventName) {
         reactContext
         .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
