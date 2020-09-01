17f1aba7274b39b6ead78f122e1a2ed322972997
Dusan Punosevac <dusan.punosevac@gmail.com>
2020-09-01 14:07:07 +0200

    Add support for RN headers in both namespaces (#248)
    Support RN headers both in the React namespace,
    where they are in RN version 0.40+,
    and no namespace, for older versions of RN
    
    Co-authored-by: Dusan Punosevac <dusan.punosevac@koye.app>

--- ios/RNBackgroundTimer.h
+++ ios/RNBackgroundTimer.h
@@ -7,7 +7,14 @@
 //
 
 #import <React/RCTBridgeModule.h>
+// Support React Native headers both in the React namespace, where they are in RN version 0.40+,
+// and no namespace, for older versions of React Native
+#if __has_include(<React/RCTEventEmitter.h>)
+#import <React/RCTEventEmitter.h>
+#else
 #import "RCTEventEmitter.h"
+#endif
+
 
 @interface RNBackgroundTimer : RCTEventEmitter <RCTBridgeModule>
 
